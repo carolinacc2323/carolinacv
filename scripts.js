8f1acefde5f5b75b4c38756cba7104638116d681
carolinacc2323 <carolinacc.tandem@gmail.com>
2024-03-13 10:55:56 +0100

    actualizar

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
-document.querySelectorAll('.printbutton').forEach(function(element) {
-    element.addEventListener('click', function() {
-        print();
-    });
-});
+// document.querySelectorAll('.printbutton').forEach(function(element) {
+//     element.addEventListener('click', function() {
+//         print();
+//     });
+// });
 
-function ocultarBoton() {
-    document.getElementById("boton").style.display = "none";
+// function ocultarBoton() {
+//     document.getElementById("boton").style.display = "none";
+// }
+// window.onbeforeprint = ocultarBoton;
+
+function imprimir(){
+    window.print()
 }
-window.onbeforeprint = ocultarBoton;
